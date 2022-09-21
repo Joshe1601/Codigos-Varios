815c7fd61d7254eb6b5c4138ea432d09ba97dca4
Joshe1601 <112984569+Joshe1601@users.noreply.github.com>
2022-09-20 19:16:58 -0500

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,27 @@
+#Fibonacci sin recursividad###
+def fib(n):
+    if n < 1:
+         return None
+    if n < 3:
+        return 1
+
+    elem1 = elem2 = 1
+    sum = 0
+    for i in range(3, n + 1):
+        sum = elem1 + elem2
+        elem1, elem2 = elem2, sum
+    return sum
+
+for n in range(1, 10): # probando
+    print(n, "->", fib(n))
+
+#Fibonacci con recursividad###
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+num = int(input("Ingrese un término de Fibonacci: "))
+print(fib(num))
